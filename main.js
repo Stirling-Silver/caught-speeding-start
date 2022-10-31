d6007c63f86b69e49859fdd3ce23faf719b96d95
Stirling-Silver <113132128+Stirling-Silver@users.noreply.github.com>
2022-10-31 11:23:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // Caught Speeding CYU Asgn
+let TicketEL = document.getElementById("");
+
+document.getElementById("btn").addEventListener("click", Ticket);
+
+function Ticket() {
+  let DSF = +document.getElementById("DS").value;
+
+  let SLF = +document.getElementById("SL").value;
+
+  // Process
+
+  let TicketSize = DSF - SLF;
+
+  if (TicketSize <= 0) {
+  }
+}
